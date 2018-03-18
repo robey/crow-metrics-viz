69da93fdb42813d78ac78d00078552e3a345e41e
Robey Pointer <robeypointer@gmail.com>
2018-03-18 11:25:12 -0700

    put links to the json in the main page. add a details click of min/max.

--- static/viz.js
+++ static/viz.js
@@ -2,6 +2,7 @@ async function refresh() {
   const response = await fetch("history.json");
   const history = response.ok ? await response.json() : {};
   const names = Object.keys(history).filter(name => name[0] != "@").sort();
+  const timestamps = history["@timestamp"];
 
   const graphsDiv = document.getElementById("graphs");
   while (graphsDiv.firstChild) graphsDiv.removeChild(graphsDiv.firstChild);
@@ -10,29 +11,41 @@ async function refresh() {
     const points = history[name].map((v, i) => v == null ? null : [ i, v ]).filter(p => p != null);
     // draw a straight line when we have exactly one data point:
     if (points.length == 1) points.push([ points[0][0] + 1, points[0][1] ]);
+    const pMax = rankedPoint(points, getY), pMin = rankedPoint(points, p => -getY(p));
 
     const graphDiv = document.getElementById("graph-template").cloneNode(true);
-    const setText = (name, text) => graphDiv.getElementsByClassName(name)[0].textContent = text;
+    const div = name => graphDiv.getElementsByClassName(name)[0];
     graphDiv.id = `graph-${name}`;
-    setText("name", name);
+    div("name").textContent = name;
     if (points.length == 0) {
-      setText("value", "(none)");
+      div("value").textContent = "(none)";
     } else {
-      setText("value", points[points.length - 1][1].toString().slice(0, 11));
-      drawSvg(graphDiv.getElementsByTagName("svg")[0], points);
+      div("value").textContent = points[points.length - 1][1].toString().slice(0, 11);
+      drawSvg(div("svg"), points, getY(pMax));
     }
+
+    const maxTime = iso(timestamps[getX(pMax)]).slice(11, 19);
+    const minTime = iso(timestamps[getX(pMin)]).slice(11, 19);
+    let details = `max [${maxTime}] ${getY(pMax)}<br>min [${minTime}] ${getY(pMin)}`;
+    graphDiv.addEventListener("click", e => {
+      if (div("details").textContent.length > 0) {
+        div("details").innerHTML = "";
+      } else {
+        div("details").innerHTML = details;
+      }
+    });
+
     graphsDiv.appendChild(graphDiv);
   }
 
-  document.getElementById("current-time").textContent = new Date().toISOString();
+  document.getElementById("current-time").textContent = iso(Date.now());
 }
 
-function drawSvg(svg, points) {
+function drawSvg(svg, points, ymax) {
   const xOffset = 0.5, yOffset = 0, width = svg.width.baseVal.value - xOffset * 2, height = svg.height.baseVal.value;
   const pointsToSvg = points => points.map(([ x, y ]) => `${x} ${y}`).join(", ");
 
-  const xmin = points[0][0], xmax = points[points.length - 1][0];
-  const ymax = Math.max(...points.map(([ x, y ]) => y));
+  const xmin = getX(points[0]), xmax = getX(points[points.length - 1]);
   const scaledPoints = points.map(([ x, y ]) => [
     xOffset + width * (x - xmin) / (xmax - xmin),
     (yOffset + height) - height * y / ymax
@@ -44,6 +57,14 @@ function drawSvg(svg, points) {
   const polygon = svg.getElementsByTagName("polygon")[0];
   polygon.setAttribute("points", pointsToSvg(polyPoints));
 }
+
+function rankedPoint(points, f) {
+  return [...points].sort((a, b) => f(b) - f(a))[0];
+}
+
+const getX = p => p[0];
+const getY = p => p[1];
+const iso = ts => new Date(ts).toISOString();
 
 setInterval(refresh, 5000);
 refresh();
