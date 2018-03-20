0517ed060fd51148c4b5b65f02571cb6a78980cc
Robey Pointer <robeypointer@gmail.com>
2018-03-20 16:04:24 -0700

    remember the "details" status of each graph between refreshes. make the background red if the network is down. clean up the js a bit to make it more readable.

--- static/viz.js
+++ static/viz.js
@@ -1,19 +1,45 @@
+// very simple/dumb UX to show each metric's current value and a tiny graph of its past values.
+
+const getX = p => p[0];
+const getY = p => p[1];
+const iso = ts => new Date(ts).toISOString();
+
+const showDetails = new Set();
+
 async function refresh() {
-  const response = await fetch("history.json");
-  const history = response.ok ? await response.json() : {};
+  const timeDiv = document.getElementById("current-time");
+  const graphsDiv = document.getElementById("graphs");
+  const templateDiv = document.getElementById("graph-template");
+
+  let response;
+  try {
+    response = await fetch("history.json");
+  } catch (error) {
+    console.log(error);
+    graphsDiv.classList.add("error");
+    return;
+  }
+
+  graphsDiv.classList.remove("error");
+  timeDiv.textContent = iso(Date.now());
+  if (!response.ok) {
+    graphsDiv.classList.add("error");
+    return;
+  }
+
+  const history = await response.json();
   const names = Object.keys(history).filter(name => name[0] != "@").sort();
   const timestamps = history["@timestamp"];
 
-  const graphsDiv = document.getElementById("graphs");
   while (graphsDiv.firstChild) graphsDiv.removeChild(graphsDiv.firstChild);
 
   for (const name of names) {
-    const points = history[name].map((v, i) => v == null ? null : [ i, v ]).filter(p => p != null);
+    const points = history[name].map((v, i) => [ i, v ]).filter(p => getY(p) != null);
     // draw a straight line when we have exactly one data point:
-    if (points.length == 1) points.push([ points[0][0] + 1, points[0][1] ]);
+    if (points.length == 1) points.push([ getX(points[0]) + 1, getY(points[0]) ]);
     const pMax = rankedPoint(points, getY), pMin = rankedPoint(points, p => -getY(p));
 
-    const graphDiv = document.getElementById("graph-template").cloneNode(true);
+    const graphDiv = templateDiv.cloneNode(true);
     const div = name => graphDiv.getElementsByClassName(name)[0];
     graphDiv.id = `graph-${name}`;
     div("name").textContent = name;
@@ -27,20 +53,22 @@ async function refresh() {
     const maxTime = iso(timestamps[getX(pMax)]).slice(11, 19);
     const minTime = iso(timestamps[getX(pMin)]).slice(11, 19);
     let details = `max [${maxTime}] ${getY(pMax)}<br>min [${minTime}] ${getY(pMin)}`;
+    if (showDetails.has(name)) div("details").innerHTML = details;
     graphDiv.addEventListener("click", e => {
-      if (div("details").textContent.length > 0) {
+      if (showDetails.has(name)) {
         div("details").innerHTML = "";
+        showDetails.remove(name);
       } else {
         div("details").innerHTML = details;
+        showDetails.add(name);
       }
     });
 
     graphsDiv.appendChild(graphDiv);
   }
-
-  document.getElementById("current-time").textContent = iso(Date.now());
 }
 
+// mimic the code in peity
 function drawSvg(svg, points, ymax) {
   const xOffset = 0.5, yOffset = 0, width = svg.width.baseVal.value - xOffset * 2, height = svg.height.baseVal.value;
   const pointsToSvg = points => points.map(([ x, y ]) => `${x} ${y}`).join(", ");
@@ -58,13 +86,11 @@ function drawSvg(svg, points, ymax) {
   polygon.setAttribute("points", pointsToSvg(polyPoints));
 }
 
+// return the point with the highest score for f(point)
 function rankedPoint(points, f) {
   return [...points].sort((a, b) => f(b) - f(a))[0];
 }
 
-const getX = p => p[0];
-const getY = p => p[1];
-const iso = ts => new Date(ts).toISOString();
 
 setInterval(refresh, 5000);
-refresh();
+setTimeout(refresh, 0);
